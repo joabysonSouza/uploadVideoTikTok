b7976a62c8875c080d10d9c6be28d0fe7cfba8bd
Joabyson <joabyson916@gmail.com>
2025-07-31 23:08:57 -0300

    feat:"acessando a pagina de envio de vídeos e tentando pegar um video aleatório de uma pasta especifica "

--- index.ts
+++ index.ts
@@ -1,19 +1,31 @@
-import {chromium} from "playwright"
-import path from "path"
+import path from "path";
+import fs from "fs"
+import { chromium } from "playwright";
+
+const UploadVideoTikTok = async()=>{
+   const context = await chromium.launchPersistentContext("./meu-perfil", {
+  headless: false,
+  args: ['--start-maximized', '--disable-blink-features=AutomationControlled'],
+});
+const page = await context.newPage();
+await page.goto("https://www.tiktok.com/tiktokstudio/upload?from=webapp");
+
+  const fileInput = await page.waitForSelector('input[type="file"]');
+  const arquivoVideo = path.resolve("Documentos/videos/videosTeste")
+  const filterVideo = fs.readdirSync(arquivoVideo).filter(video => video.endsWith(".mp4"))
+
+  if(filterVideo.length === 0 ){
+    console.log("Nenhum video Mp4 encontrado");
+    
+  }
+  const videoEnviarAleatorio =[Math.floor(Math.random() * filterVideo.length)]
 
 
-
- async function uploadVideos(){
-    const browser = await chromium.launch({
-        headless: false
-    })
-
-    const context = await browser.newContext()
-    const page = await browser.newPage()
-
-      await page.goto("https://www.tiktok.com/upload");
-
-        
 }
 
-uploadVideos()
+UploadVideoTikTok()
+  
+
+
+
+ 
